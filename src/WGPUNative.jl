8e8cb3997a4927605d65cdd9596d5abe4e2d980a
Arhik <arhik23@gmail.com>
2024-03-30 22:58:08 +0530

    remove redundant CString functions
    remove unnecessary string conversion functions

--- src/WGPUNative.jl
+++ src/WGPUNative.jl
@@ -1,17 +1,5 @@
 module WGPUNative
 
-function toCString(s::String)
-	sNullTerminated = s*"\0"
-	sPtr = GC.@preserve sNullTerminated pointer(sNullTerminated)
-	dPtr = Libc.malloc(sizeof(sNullTerminated))
-	dUInt8Ptr = convert(Ptr{UInt8}, dPtr)
-	unsafe_copyto!(dUInt8Ptr, sPtr, sizeof(sNullTerminated))
-end
-
-function fromCString(s::Ptr{Int8})
-    s != C_NULL ? unsafe_string(s) : ""
-end
-
 using Reexport
 include("LibWGPU.jl")
 @reexport using .LibWGPU
