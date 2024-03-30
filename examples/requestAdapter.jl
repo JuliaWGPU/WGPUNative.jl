5a42dc390127b211007361b9b69f22db73d470c2
Arhik <arhik23@gmail.com>
2024-03-30 22:46:49 +0530

    Update requestAdapter.jl

--- examples/requestAdapter.jl
+++ examples/requestAdapter.jl
@@ -1,88 +1,36 @@
 ## Load WGPU
 using WGPUNative
 
-function logCallBack(logLevel::WGPULogLevel, msg::Ptr{Cchar})
-		if logLevel == WGPULogLevel_Error
-				level_str = "ERROR"
-		elseif logLevel == WGPULogLevel_Warn
-				level_str = "WARN"
-		elseif logLevel == WGPULogLevel_Info
-				level_str = "INFO"
-		elseif logLevel == WGPULogLevel_Debug
-				level_str = "DEBUG"
-		elseif logLevel == WGPULogLevel_Trace
-				level_str = "TRACE"
-		else
-				level_str = "UNKNOWN LOG LEVEL"
-		end
-        println("$(level_str) $(unsafe_string(msg))")
-end
-
-logcallback = @cfunction(logCallBack, Cvoid, (WGPULogLevel, Ptr{Cchar}))
-
-wgpuSetLogCallback(logcallback, Ptr{Cvoid}())
-wgpuSetLogLevel(WGPULogLevel_Debug)
-
-adapter = Ref{WGPUAdapter}()
-
-chain = cStruct(
-    WGPUChainedStruct;
-    sType = WGPUSType(Int64(WGPUSType_AdapterExtras)),
-)
-
-
-extras = cStruct(
-    WGPUAdapterExtras;
-    backend=WGPUBackendType_Vulkan, # TODO hardcoding on windows for now
-    chain = chain |> concrete
-)
-
-# extras.chain = chain |> concrete
-
-adapterOptions = cStruct(WGPURequestAdapterOptions)
-adapterOptions.compatibleSurface = C_NULL
-adapterOptions.nextInChain = rawCast(WGPUChainedStruct, extras)
-adapterOptions.powerPreference = WGPUPowerPreference_HighPerformance
-adapterOptions.forceFallbackAdapter = false
+adapter = WGPUAdapter()
 
 function request_adapter_callback(
 				status::WGPURequestAdapterStatus,
 				returnAdapter::WGPUAdapter,
 			   message::Ptr{Cchar},
 			   userData::Ptr{Cvoid})
-    global adapter
-    # @debug status
-    if status == WGPURequestAdapterStatus_Success
-        adapter[] = returnAdapter
-    elseif message != C_NULL
-        @error unsafe_string(message)
-    end
-	return nothing
+    global adapter = returnAdapter
+    return nothing
 end
 
 requestAdapterCallback = @cfunction(request_adapter_callback, Cvoid, (WGPURequestAdapterStatus, WGPUAdapter, Ptr{Cchar}, Ptr{Cvoid}))
-## request adapter 
-instanceDesc = cStruct(WGPUInstanceDescriptor)
-instanceDesc.nextInChain = C_NULL
 
-instance = wgpuCreateInstance(instanceDesc  |> ptr)
+instance = wgpuCreateInstance(C_NULL)
 
-wgpuInstanceRequestAdapter(instance, 
-						   adapterOptions |> ptr, 
-						   requestAdapterCallback,
-						   C_NULL)
+wgpuInstanceRequestAdapter(
+	instance, 
+	C_NULL, 
+	requestAdapterCallback,
+	adapter
+)
 
-@assert adapter[] != C_NULL
+@assert adapter != C_NULL
 
-properties = cStruct(WGPUAdapterProperties)
+properties = WGPUAdapterProperties()
 
-wgpuAdapterGetProperties(adapter[], properties |> ptr)
+wgpuAdapterGetProperties(adapter, pointer_from_objref(properties))
 
-supportedLimits = cStruct(WGPUSupportedLimits)
-cLimits = supportedLimits.limits
+supportedLimits = WGPUSupportedLimits()
 
-wgpuAdapterGetLimits(adapter[], supportedLimits |> ptr)
-
-
+wgpuAdapterGetLimits(adapter, pointer_from_objref(supportedLimits))
 
 
