a902c1d04d5388a58bb88e6dd61d4f28a149c333
Arhik <arhik23@gmail.com>
2024-03-31 11:34:55 +0530

    Update enumerateAdapter.jl

--- examples/enumerateAdapter.jl
+++ examples/enumerateAdapter.jl
@@ -14,17 +14,25 @@ wgpuInstanceEnumerateAdapters(instance, C_NULL, adapters |> pointer)
 for (idx, adapter) in enumerate(adapters)
 	properties = WGPUAdapterProperties()
 	wgpuAdapterGetProperties(adapter, properties |> pointer_from_objref)
+	vendorID = properties.vendorID |> Int
+	architecture = properties.architecture |> unsafe_string
+	deviceID = properties.deviceID |> Int
+	name = properties.name |> unsafe_string
+	vendorName = properties.vendorName |> unsafe_string
+	description = properties.driverDescription |> unsafe_string
+	adapterType = properties.adapterType
+	backendType = properties.backendType
 	@info(
 		adapter, 
 		idx, 
-		properties.vendorID,
-		properties.vendorName,
-		properties.architecture,
-		properties.deviceID,
-		properties.name,
-		properties.driverDescription,
-		properties.adapterType,
-		properties.backendType
+		vendorID,
+		vendorName,
+		architecture,
+		deviceID,
+		name,
+		description,
+		adapterType,
+		backendType
 	)
 	wgpuAdapterRelease(adapter)
 end
